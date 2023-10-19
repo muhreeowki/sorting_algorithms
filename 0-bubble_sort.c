59eed75aa7e2802342ce1faada338b7293cc0292
Muriuki <muriuki@muchiri.com>
2023-10-19 15:07:54 +0300

    Added task 0: Bubble Sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * bubble_sort - sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ *
+ * @array - array to sort
+ * @size_t - size of the array
+ *
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+  size_t i, j;
+  int temp;
+
+
+  for (j = 0; j < size; j++)
+  {
+    for (i = 0; i < size; i++) {
+      if (array[i] > array[i+1])
+      {
+        temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+        print_array(array, size);
+      }
+    }
+  }
+}
